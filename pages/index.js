54f82b756c23382918de19c44da27a0715c7b8bf
aotopkov <aotopkov@gmail.com>
2022-07-01 18:09:53 +0300

    bugfix1

--- pages/index.js
+++ pages/index.js
@@ -26,21 +26,6 @@ const сards = [
   }
 ];
 
-//открытие попапов
-function openPopup(popup) {
-  popup.style.opacity = '1';
-  popup.style.visibility = 'visible';
-}
-
-//закрытие попапов
-
-function closePopup(popup) {
-  popup.style.opacity = '0';
-  popup.style.visibility = 'hidden';
-}
-
-
-// popup Profile Edit
 const profileEditBtn = document.querySelector(".profile__btn-edit");
 const popupProfile = document.querySelector(".popup_section_profile");
 const profileName = document.querySelector(".profile__data-name");
@@ -48,35 +33,7 @@ const profileAbout = document.querySelector(".profile__data-about");
 const profileNameEdit = document.querySelector(".popup__profile-name");
 const profileAboutEdit = document.querySelector(".popup__profile-about");
 const profileEditForm = document.querySelector('.popup__forms_section_profile')
-const profileBtnExit = document.querySelector('.popup__btn-exit');
 
-profileEditBtn.addEventListener('click', () => {
-  openPopup(popupProfile); 
-  profileNameEdit.value = profileName.textContent;
-  profileAboutEdit.value = profileAbout.textContent;
-})
-
-function resetProfileData() {
-  profileNameEdit.value = "";
-  profileAboutEdit.value = "";
-}
-
-function editProfile(evt) {
-  evt.preventDefault();
-  profileName.textContent = profileNameEdit.value;
-  profileAbout.textContent = profileAboutEdit.value;
-  closePopup(popupProfile)
-  resetProfileData()
-}
-
-profileEditForm.addEventListener("submit", editProfile);
-
-profileBtnExit.addEventListener('click', () => {
-  closePopup(popupProfile);
-  resetProfileData();
-})
-
-//popup Card Edit
 const popupCardAdd = document.querySelector('.card__btn-add')
 const popupCard = document.querySelector(".popup_section_card");
 const cardBtnExit = document.querySelector(".popup__btn-exit_section_card");
@@ -86,27 +43,59 @@ const cardAddForm = document.querySelector('.popup__forms_section_card')
 const fullCardPopup = document.querySelector('.fullcard');
 const fullCardName = document.querySelector('.fullcard__name');
 const fullCardImg = document.querySelector('.fullcard__img');
-const fullCardExit = document.querySelector('.fullcard__btn-exit')
 
-//добавление карточек
+const cardContainer = document.querySelector('.cards');
+const templateCard = document.querySelector('.template__card').content;
+
+const btnExit = document.querySelectorAll('.popup__btn-exit');
+
+//открытие попапов
+function openPopup(popup) {
+  popup.classList.add('popup_opened');
+}
+
+//закрытие попапов
+
+function closePopup(popup) {
+  popup.classList.remove('popup_opened');
+}
+
+btnExit.forEach((btn) => {
+  const popup = btn.closest('.popup');
+  btn.addEventListener('click', () => {closePopup(popup)}) 
+})
+
+//Profile Edit
+
+profileEditBtn.addEventListener('click', () => {
+  openPopup(popupProfile); 
+  profileNameEdit.value = profileName.textContent;
+  profileAboutEdit.value = profileAbout.textContent;
+})
+
+function editProfile(evt) {
+  evt.preventDefault();
+  profileName.textContent = profileNameEdit.value;
+  profileAbout.textContent = profileAboutEdit.value;
+  closePopup(popupProfile)
+}
+
+profileEditForm.addEventListener("submit", editProfile);
+
+
+//Card Add
 
 popupCardAdd.addEventListener('click', () => {
   openPopup(popupCard);
 })
 
-function resetCardData () {
-  cardNameEdit.value = "";
-  cardLinkEdit.value = "";
-}
 
-function addCard(evt) {
-  evt.preventDefault();
-  const cardContainer = document.querySelector('.cards');
-  const templateCard = document.querySelector('.template__card').content;
+
+function createCard (name, link) {
   const cardElement = templateCard.querySelector('.card').cloneNode(true);
-
-  cardElement.querySelector('.card__name').textContent = cardNameEdit.value;
-  cardElement.querySelector('.card__image').setAttribute('src', cardLinkEdit.value)
+  cardElement.querySelector('.card__name').textContent = name;
+  cardElement.querySelector('.card__image').setAttribute('src', link)
+  cardElement.querySelector('.card__image').setAttribute('alt', name)
   cardElement.querySelector('.card__like').addEventListener('click', (evt) => {
     evt.target.classList.toggle('card__like_active');
   })
@@ -115,44 +104,26 @@ function addCard(evt) {
   })
   cardElement.querySelector('.card__full-btn').addEventListener('click', () => {
     openPopup(fullCardPopup);
-    fullCardName.textContent = cardElement.querySelector('.card__name').textContent;
-    fullCardImg.setAttribute('src', cardElement.querySelector('.card__image').getAttribute('src'))
-    fullCardExit.addEventListener('click', () => {closePopup(fullCardPopup)})
+    fullCardName.textContent = name;
+    fullCardImg.setAttribute('src', link)
+    fullCardImg.setAttribute('alt', name)
   })
-  cardContainer.append(cardElement);
+  return cardElement;
+}
 
+function addCard(evt) {
+  evt.preventDefault();
+  cardContainer.prepend(createCard(cardNameEdit.value, cardLinkEdit.value));
   closePopup(popupCard);
-  resetCardData(); 
+  evt.target.reset(); 
 }
 
 cardAddForm.addEventListener('submit', addCard);
 
-cardBtnExit.addEventListener('click', () => {
-  closePopup(popupCard);
-  resetCardData();
-})
 
 // добавление карточек из массива
 //обход массива
 
 сards.forEach(element => {
-  const cardContainer = document.querySelector('.cards');
-  const templateCard = document.querySelector('.template__card').content;
-  const cardElement = templateCard.querySelector('.card').cloneNode(true);
-
-  cardElement.querySelector('.card__name').textContent = element.name;
-  cardElement.querySelector('.card__image').setAttribute('src', element.link)
-  cardElement.querySelector('.card__like').addEventListener('click', (evt) => {
-    evt.target.classList.toggle('card__like_active');
-  })
-  cardElement.querySelector('.card__delete').addEventListener('click', () => {
-    cardElement.closest('.card').remove();
-  })
-  cardElement.querySelector('.card__full-btn').addEventListener('click', () => {
-    openPopup(fullCardPopup)
-    fullCardName.textContent = cardElement.querySelector('.card__name').textContent;
-    fullCardImg.setAttribute('src', cardElement.querySelector('.card__image').getAttribute('src'))
-    fullCardExit.addEventListener('click', () => {closePopup(fullCardPopup)})
-  })
-  cardContainer.append(cardElement);
+  cardContainer.append(createCard(element.name, element.link));
 });
