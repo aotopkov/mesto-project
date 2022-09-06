c3eb41e01ff638dd13e80f7ae02be7c976143fbc
aotopkov <aotopkov@gmail.com>
2022-09-07 01:39:01 +0300

    edit: Readme и исправления по чеклисту

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import '../pages/index.css';
+import {initProfileData, popupListeners} from './modal.js';
+import {enableValidation} from './validate.js';
+
+initProfileData()
+popupListeners()
+enableValidation()
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './pages/index.css';
-import {initProfileData, popupListeners} from './components/modal.js';
-import {enableValidation} from './components/validate.js';
-
-initProfileData()
-popupListeners()
-enableValidation()
